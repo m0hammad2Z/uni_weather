50e2190f042c0aea4f278aa7d0b3d80582a8c54b
Mohammad <imhamd33@gmail.com>
2022-03-28 16:30:06 +0300

    Update AppLibrary.dart

--- lib/AppLibrary.dart
+++ lib/AppLibrary.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:weather/weather.dart';
 
-//Hola Casseta
-
 String? selectedValue;
 List<String> items = [
   'Hashemite University HU',
